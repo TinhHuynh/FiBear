35eb920aaecc5eadf757216016d23eae2a1ac8a6
tnqv102 <tnqv1021997@gmail.com>
2018-03-25 20:18:57 +0700

    VuTNQ - Update instruction and how to push refresh token - Update notification when receive message

--- app/src/main/java/com/fibear/android/fibear/service/firebase/FiBearInstanceIdService.kt
+++ app/src/main/java/com/fibear/android/fibear/service/firebase/FiBearInstanceIdService.kt
@@ -1,5 +1,7 @@
 package com.fibear.android.fibear.service.firebase
 
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.FirebaseInstanceIdService
 
 
@@ -9,6 +11,24 @@ import com.google.firebase.iid.FirebaseInstanceIdService
 class FiBearInstanceIdService : FirebaseInstanceIdService() {
 
     override fun onTokenRefresh() {
+        super.onTokenRefresh()
+        // Get updated InstanceID token.
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d("FirebaseIDService", "Refreshed token: " + refreshedToken!!)
+        //RefreshToken này nó sẽ chạy đầu tiên khi mày mở app lên
+        // => nếu user chưa login thì mày lưu cái refreshedToken này xuống SharedPreference với key là gì đó
+        // => Sau khi user login success , mày gọi call API này lên
+        //post : http://35.229.53.76/v1/auth/refreshToken
+        //{"userId":1,"token":"fW_aHIS8YDk:APA91bEfJnb2QY0DGo8MlnfoiDMMtX6L0oC0Q3lEN6jX8avJTPoZ1yQWXn2HVNcPt6byWMFEHOdyqGYPvnChlrllFs_tT8fHPq2KA9OwILRcLJv64AAiKrIy4rGg4pK0WtFyZs67U_w_"}
+        // userId là user nó login vào , token chính là token mày lưu vào sharedpreferenced trước đó
+        // Thông báo bên messaging tao làm sẵn hết rồi , giờ chỉ cần gửi token lên thôi nha !
+        sendRegistrationToServer(refreshedToken)
+
+
+    }
+
+    private fun sendRegistrationToServer(refreshedToken: String?) {
+        Log.v("Sending to server!!", "Sending==" + refreshedToken!!)
     }
 
 }
--- app/src/main/java/com/fibear/android/fibear/service/firebase/FiBearMessagingService.kt
+++ app/src/main/java/com/fibear/android/fibear/service/firebase/FiBearMessagingService.kt
@@ -1,8 +1,40 @@
 package com.fibear.android.fibear.service.firebase
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import com.fibear.android.fibear.R
+import com.fibear.android.fibear.view.main.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class FiBearMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        Log.d(TAG, "From: " + p0!!.from)
+        Log.d(TAG, "Notification Message Body: " + p0.notification.body!!)
+        sendNotification(p0)
 
+    }
+
+    private fun sendNotification(remoteMessage: RemoteMessage) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+                .setContentText(remoteMessage.notification.body)
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
 
 }
